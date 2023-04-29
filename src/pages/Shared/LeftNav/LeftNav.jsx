6ba9502f02a6eabdca70a0f9af686354c85e5c1a
mh akash <mhakash5000@gmail.com>
2023-04-29 11:17:51 +0600

    left navbar added

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
 
 const LeftNav = () => {
+    const [categories,setCategories] = useState([]);
+    
+    useEffect(()=>{
+        fetch("http://localhost:5000/categories")
+          .then((res) => res.json())
+          .then((data) => setCategories(data))
+          .catch((error) => console.log(error));
+    },[])
+
     return (
-        <div>
-            LeftNav
-        </div>
+      <div className='ps-4'>
+        <h4>All Categories</h4>
+        {categories.map((category) => (
+          <p>
+            <Link to={`/category/${category.id}`} className='text-decoration-none text-dark'>{category.name}</Link>
+          </p>
+        ))}
+      </div>
     );
 };
 
